2592616efa01e8db15a9db5e31dc856205d7b55c
Sarah Nasir <109743357+Sarah072@users.noreply.github.com>
2023-07-24 22:24:07 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import ChatbotUI from "./ChatbotUI";
+import React from 'react';
+
+function App() {
+  return (
+    <ChatbotUI />
+  );
+}
+
+export default App;
---
+++ ChatbotUI.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './chatbot.css';
+import bot from './botIcon.jpg'
+
+const ChatbotUI = (props) => {
+  const [messages, setMessages] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [chatVisible, setChatVisible] = useState(false); // Track the visibility of the chat
+
+  useEffect(() => {
+    const initializeChatbot = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/init'); // Use the correct server URL
+        setMessages(response.data.messages);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    initializeChatbot();
+  }, []);
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (inputText.trim() !== '') {
+      try {
+        const response = await axios.post('http://localhost:5000/api/sendMessage', { text: inputText }); // Use the correct server URL
+        setMessages([
+          ...messages,
+          { sender: 'User', text: inputText },
+          { sender: 'Bot', text: response.data },
+        ]);
+        setInputText('');
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const toggleChatVisibility = () => {
+    setChatVisible(!chatVisible);
+  };
+
+  return (
+    
+    <div className="chat-container">
+      {chatVisible && (
+        <div className='chatbot'>
+          <div className="messages-container">
+            {messages.map((message, index) => (
+              <div key={index} className={`message ${message.sender}`}>
+                <span>{message.sender}</span>
+                <p>{message.text}</p>
+              </div>
+            ))}
+          </div>
+          <form className="input-container" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Type your message..."
+              value={inputText}
+              onChange={handleInputChange}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      )}
+      <div className="chat-icon" onClick={toggleChatVisibility}>
+        <img className="botImg" src={bot} alt="Chat" />
+      </div>
+    </div>
+  );
+}
+
+export default ChatbotUI;
